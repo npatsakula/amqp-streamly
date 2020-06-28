e94a75566369cb2bb3a971771349fa97b7f057ff
Gautier DI FOLCO <gautier.difolco@gmail.com>
2020-06-28 18:17:08 +0200

    Finish up the library

--- src/Network/AMQP/Streamly.hs
+++ src/Network/AMQP/Streamly.hs
@@ -1,7 +1,10 @@
 {-# LANGUAGE FlexibleContexts #-}
 
 module Network.AMQP.Streamly
-    ( SendInstructions(..)
+    (
+    -- * How to use this library
+    -- $use
+      SendInstructions(..)
     , produce
     , consume
     ) where
@@ -15,26 +18,44 @@ import Streamly
 import qualified Streamly.Internal.Prelude as S
 import qualified Streamly.Prelude as S
 
+-- See @Network.AMQP.publishMsg'@ for options
 data SendInstructions = SendInstructions { exchange :: Text, routingKey :: Text, mandatory :: Bool, message :: Message } deriving (Show)
 type Queue = Text
 
--- | Publish the produced messages.
+-- | Publish the produced messages
 produce :: (IsStream t, MonadAsync m) => Channel -> t m SendInstructions -> t m ()
 produce channel = S.mapM send
   where send i = liftIO $ do
                   publishMsg' channel (exchange i) (routingKey i) (mandatory i) (message i)
                   return ()
 
--- | Stream messages from a queue.
+-- | Stream messages from a queue
+--
+-- See @Network.AMQP.consumeMsgs@ for options
 consume :: (IsStream t, MonadAsync m) => Channel -> Queue -> Ack -> t m (Message, Envelope)
 consume channel queue ack = S.concatM $ liftIO $ do
   mvar <- newEmptyMVar
-  consumeMsgs channel queue NoAck $ putMVar mvar
+  consumeMsgs channel queue Ack $ putMVar mvar
   return $ S.repeatM $ taking mvar
   where taking :: MonadIO m => MVar (Message, Envelope) -> m (Message, Envelope)
-        taking mvar = liftIO $ if ack == Ack
+        taking mvar = liftIO $ if ack == NoAck
                                  then do
                                         retrieved <- takeMVar mvar
                                         ackEnv $ snd retrieved
                                         return retrieved
                                  else takeMVar mvar
+
+
+-- $use
+--
+-- This section contains basic step-by-step usage of the library.
+--
+-- You can either build a producer, which will publish all the messages of
+-- a stream:
+--
+-- > Streamly.drain $ produce channel sendInstructionsStream
+--
+-- Or a consumer, which will contain the @Message@s and @Envelope@s of
+-- a queue:
+--
+-- > Streamly.drain $ consume channel aQueue NoAck
--- test/Network/AMQP/StreamlySpec.hs
+++ test/Network/AMQP/StreamlySpec.hs
@@ -5,22 +5,23 @@ module Network.AMQP.StreamlySpec (main, spec) where
 import Network.AMQP.Streamly
 
 import Control.Concurrent(threadDelay)
-import Control.Exception(bracket)
 import Control.Monad.IO.Class(liftIO)
 
-import Docker.Client
 import Network.AMQP
 import Test.Hspec
+import TestContainers.Hspec
 import qualified Data.ByteString.Lazy.Char8 as B
+import Data.Maybe(fromJust)
 import qualified Data.Text as T
+import System.Process(readProcess)
 import qualified Streamly.Prelude as S
 
 main :: IO ()
 main = hspec spec
 
 spec :: Spec
-spec = around withChannel $
-  it "producing 5 messages should be consumed in the right order" $ \(_, channel) ->
+spec = around (withContainers containers) $
+  it "producing 5 messages should be consumed in the right order" $ \channel ->
     flip shouldReturn messages $ do
       let arbitraryExchange = "arbitraryExchange"
       let arbitraryRoutingKey = "arbitraryRoutingKey"
@@ -28,60 +29,32 @@ spec = around withChannel $
       let fixedInstructions = SendInstructions arbitraryExchange arbitraryRoutingKey True
       liftIO $ declareExchange channel newExchange { exchangeName = arbitraryExchange, exchangeType = "fanout" }
       liftIO $ declareQueue channel newQueue { queueName = arbitraryQueue }
-      liftIO $ bindQueue channel arbitraryQueue arbitraryQueue arbitraryRoutingKey
+      liftIO $ bindQueue channel arbitraryQueue arbitraryExchange arbitraryRoutingKey
       S.drain $ produce channel $ S.fromList $ map fixedInstructions messages
-      S.toList (S.take (length messages) $ fst <$> consume channel arbitraryQueue Ack)
+      S.toList (S.take (length messages) $ fst <$> consume channel arbitraryQueue NoAck)
 
 messages :: [Message]
 messages = map toMessage ["Lorem", "ipsum", "dolor", "sit", "amet"]
   where toMessage x = Message (B.pack x) Nothing Nothing Nothing Nothing Nothing Nothing Nothing Nothing Nothing Nothing Nothing Nothing Nothing Nothing
 
-open :: IO ((ContainerID, Connection), Channel)
-open = do
-  let (login, password) = ("streamly", "stream")
-  (containerId, containerDetails) <- runRabbitMQContainer login password
-  let hostName = T.unpack $ networkSettingsIpAddress $ networkSettings containerDetails
-  connection <- openConnection hostName "/" login password
-  channel <- openChannel connection
-  return ((containerId, connection), channel)
+mkChannel :: T.Text -> T.Text -> String -> IO Channel
+mkChannel login password ip = do
+  connection <- openConnection ip "/" login password
+  openChannel connection
 
-runRabbitMQContainer :: T.Text -> T.Text -> IO (ContainerID, ContainerDetails)
-runRabbitMQContainer login password = do
-    h <- defaultHttpHandler
-    runDockerT (defaultClientOpts, h) $ do
-      let envVars = [EnvVar "RABBITMQ_DEFAULT_USER" login, EnvVar "RABBITMQ_DEFAULT_PASS" password]
-      let baseCreationOptions = defaultCreateOpts "rabbitmq:3.8.4"
-      let creationOptions = baseCreationOptions { containerConfig = (containerConfig baseCreationOptions) { env = envVars <> env (containerConfig baseCreationOptions) } }
-      cid <- createContainer creationOptions Nothing
-      case cid of
-          Left err -> error $ show err
-          Right containerId -> do
-              _ <- startContainer defaultStartOpts containerId
-              containerDetails <- fetchContainerDetails containerId
-              return (containerId, containerDetails)
+getIp :: Container -> IO String
+getIp (Container id _ _) =
+  format <$> readProcess "docker" [ "inspect"
+                                  , "-f"
+                                  , "'{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}'"
+                                  , T.unpack id
+                                  ] ""
+  where format = init . init . tail
 
-fetchContainerDetails containerId = do
-  details <- inspectContainer containerId
-  case details of
-    Right x -> return x
-    Left _  -> do
-      liftIO $ threadDelay 1000000
-      fetchContainerDetails containerId
-
-close :: ((ContainerID, Connection), Channel) -> IO ()
-close ((containerId, connection), channel) = do
-  stopRabbitMQContainer containerId
-  closeChannel channel
-  closeConnection connection
-
-stopRabbitMQContainer :: ContainerID -> IO ()
-stopRabbitMQContainer cid = do
-    h <- defaultHttpHandler
-    runDockerT (defaultClientOpts, h) $ do
-      r <- stopContainer DefaultTimeout cid
-      case r of
-          Left err -> error "I failed to stop the container"
-          Right _ -> return ()
-
-withChannel :: (((ContainerID, Connection), Channel) -> IO ()) -> IO ()
-withChannel = bracket open close
+containers :: MonadDocker m => m Channel
+containers = do
+  let (login, password) = ("guest", "guest")
+  container <- run (fromTag "rabbitmq:3.8.4") defaultContainerRequest
+  liftIO $ threadDelay $ 15 * 1000 * 1000
+  ip <- liftIO $ getIp container
+  liftIO $ mkChannel login password ip
